6101878d5a25ac23b3b703425b2d9d40f3ca0e32
JiahaoZhan <Ichirojp201@gmail.com>
2023-08-19 07:19:25 -0400

    remove unnecessary slash at the beginning

--- grocery/urls.py
+++ grocery/urls.py
@@ -24,8 +24,8 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/v1/token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('/v1/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
-    path('/v1/token/verify', TokenVerifyView.as_view(), name='token_verify'),
-    path('/v1/', include('shoppinglist.urls')),
+    path('v1/token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('v1/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
+    path('v1/token/verify', TokenVerifyView.as_view(), name='token_verify'),
+    path('v1/', include('shoppinglist.urls')),
 ]
